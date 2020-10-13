483bbc2f058ef7c9ca921a5b83d0ebfefe07f8b4
Daniel Mouris <dgmouris@gmail.com>
2020-10-12 22:40:53 -0600

    Modified settings for vue project

--- server/server/settings.py
+++ server/server/settings.py
@@ -86,6 +86,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -168,4 +169,8 @@ CORS_ALLOW_CREDENTIALS = True
 
 # NOTE:
 # change 'https://example-prod-vue.com' to your frontend domain
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://example-prod-vue.com']
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'https://example-prod-vue.com'
+]
